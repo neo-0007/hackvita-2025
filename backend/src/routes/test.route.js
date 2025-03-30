5c11e481019b284b2478a6e1288c2fd889675c56
Rahul Yadav <hulracoc12345@gmail.com>
2025-03-31 02:07:45 +0530

    added feature to update the weak topics of the user after the test

--- backend/src/routes/test.route.js
+++ backend/src/routes/test.route.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { register, login, getUserDetails, profile, logout } = require('../controllers/auth.controller');
 const { protectRoute } = require('../middleware/auth.middleware');
-const { get_10_questions } = require('../controllers/test.controller');
+const { get_10_questions, update_user_topics } = require('../controllers/test.controller');
 
 router.post('/generate', get_10_questions);
+router.post('/update', update_user_topics)
 
 module.exports = router;
